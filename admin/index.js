d85a28e7d06b2c160247e7bd0189684eb30aff85
João Victor Alves <joaovba2002@alu.ufc.br>
2022-06-29 12:22:09 -0300

    admin package

---
+++ admin/index.js
@@ -0,0 +1,21 @@
+"use strict;"
+
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+
+const port = 3030;
+const address = "localhost";
+
+const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use('/', routes);
+
+const server = app.listen(port, address, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log(`Servidor executando no endereço ${host} e na porta ${port}`);
+});
