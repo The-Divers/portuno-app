e61c1c3538f67ff71f2ef2da16153e7539ecc5c7
João Victor Alves <joaovba2002@alu.ufc.br>
2022-07-12 00:15:05 -0300

    Modificações no fluxo das rotas

--- front/routes.js
+++ front/routes.js
@@ -4,31 +4,33 @@ const rooms = require("./model/Rooms");
 const router = express.Router();
 const { getRooms, getOneRoom, updateRoom } = require('./model/Rooms')
 const { getReservations, getOneReservation, updateReservation } = require('./model/Reservation')
-const { users, userApp } = require('./model/Users')
-
+const { userApp } = require('./model/Users')
 
 router.use(express.static('public'));
 
-// ROTAS DE PÁGINAS
+/* <<< ROTAS DE PÁGINAS >>> */
+
 router.get('/', (req, res) => {
     res.render("pages/login");
 });
 
 router.get('/home', async (req, res) => {
+    //Recebe todas as salas
     let rooms = await getRooms();
     //Remove salas de outros andares
     for (var i = rooms.length - 1; i >= 0; i--) {
         if (rooms[i].data.position != 0) {
-            let room = rooms[i];
             rooms.splice(i, 1);
         }
     }
+    //Ordena as salas
     rooms = rooms.sort((a, b) => a.data.order - b.data.order)
     res.render("pages/home", { rooms: rooms, filter: false });
 
 });
 
 router.get('/andar1', async (req, res) => {
+    //Recebe todas as salas
     let rooms = await getRooms();
     //Remove salas de outros andares
     for (var i = rooms.length - 1; i >= 0; i--) {
@@ -37,13 +39,14 @@ router.get('/andar1', async (req, res) => {
             rooms.splice(i, 1);
         }
     }
+    //Ordena as salas
     rooms = rooms.sort((a, b) => a.data.order - b.data.order)
     res.render("pages/andar1", { rooms: rooms, filter: false });
 });
 
 router.get('/andar2', async (req, res) => {
+    //Recebe todas as salas
     let rooms = await getRooms();
-
     //Remove salas de outros andares
     for (var i = rooms.length - 1; i >= 0; i--) {
         if (rooms[i].data.position != 2) {
@@ -51,6 +54,7 @@ router.get('/andar2', async (req, res) => {
             rooms.splice(i, 1);
         }
     }
+    //Ordena as salas
     rooms = rooms.sort((a, b) => a.data.order - b.data.order)
     res.render("pages/andar2", { rooms: rooms, filter: false });
 });
@@ -60,11 +64,13 @@ router.get('/horarios', (req, res) => {
 });
 
 router.get('/reservas', async (req, res) => {
+    //Recebe as reservas
     const reservations = await getReservations();
     let user = userApp;
 
     let myReservation = null;
 
+    //Mostra apenas as reservas relacionadas ao usuário
     if (reservations != null) {
         reservations.forEach(reservation => {
             if (user.uid == reservation.data.uid && reservation.data.status == "PERMITIDO") {
@@ -73,7 +79,7 @@ router.get('/reservas', async (req, res) => {
             }
         });
     }
-
+    //Renderiza a página de reservas
     res.render("pages/reservas", { reservation: myReservation });
 });
 
@@ -82,173 +88,23 @@ router.get('/perfil', (req, res) => {
 });
 
 
+/* <<< ROTAS DE AÇÃO >>> */
 
-
-
-
-
-
-
-
-
-//ROTAS DE FILTROS
-router.post('/home', async (req, res) => {
-    let data = req.body
-
-    let rooms = await getRooms();
-    let filterRooms = [];
-    //Remove salas de outros andares
-    for (var i = rooms.length - 1; i >= 0; i--) {
-        if (rooms[i].data.position != 0) {
-            rooms.splice(i, 1);
-        }
-    }
-    //Adiciona a Salas Filtradas as salas com os  de tipo
-    if (data.filter1 != undefined) {
-        for (var i = rooms.length - 1; i >= 0; i--) {
-            if (rooms[i].data.roomType == data.filter1) {
-                filterRooms.push(rooms[i]);
-            }
-        }
-        if (data.filter2 != undefined) {
-            for (var i = filterRooms.length - 1; i >= 0; i--) {
-                if (filterRooms[i].data.status == data.filter2) {
-                } else {
-                    filterRooms.splice(i, 1);
-                }
-            }
-        }
-    } else {
-        if (data.filter2 != undefined) {
-            for (var i = rooms.length - 1; i >= 0; i--) {
-                if (rooms[i].data.status == data.filter2) {
-                } else {
-                    rooms.splice(i, 1);
-                }
-            }
-        }
-        filterRooms = rooms;
-    }
-
-    filterRooms = filterRooms.sort((a, b) => a.data.order - b.data.order)
-    res.render("pages/home", { rooms: filterRooms, filter: true });
-
-});
-
-router.post('/andar1', async (req, res) => {
-    let data = req.body
-
-    let rooms = await getRooms();
-    let filterRooms = [];
-    //Remove salas de outros andares
-    for (var i = rooms.length - 1; i >= 0; i--) {
-        if (rooms[i].data.position != 1) {
-            rooms.splice(i, 1);
-        }
-    }
-    //Adiciona a Salas Filtradas as salas com os  de tipo
-    if (data.filter1 != undefined) {
-        for (var i = rooms.length - 1; i >= 0; i--) {
-            if (rooms[i].data.roomType == data.filter1) {
-                filterRooms.push(rooms[i]);
-            }
-        }
-        if (data.filter2 != undefined) {
-            for (var i = filterRooms.length - 1; i >= 0; i--) {
-                if (filterRooms[i].data.status == data.filter2) {
-                } else {
-                    filterRooms.splice(i, 1);
-                }
-            }
-        }
-    } else {
-        if (data.filter2 != undefined) {
-            for (var i = rooms.length - 1; i >= 0; i--) {
-                if (rooms[i].data.status == data.filter2) {
-                } else {
-                    rooms.splice(i, 1);
-                }
-            }
-        }
-        filterRooms = rooms;
-    }
-
-    filterRooms = filterRooms.sort((a, b) => a.data.order - b.data.order)
-    res.render("pages/andar1", { rooms: filterRooms, filter: true });
-
-});
-
-router.post('/andar2', async (req, res) => {
-    let data = req.body
-
-    let rooms = await getRooms();
-    let filterRooms = [];
-    //Remove salas de outros andares
-    for (var i = rooms.length - 1; i >= 0; i--) {
-        if (rooms[i].data.position != 2) {
-            rooms.splice(i, 1);
-        }
-    }
-    //Adiciona a Salas Filtradas as salas com os  de tipo
-    if (data.filter1 != undefined) {
-        for (var i = rooms.length - 1; i >= 0; i--) {
-            if (rooms[i].data.roomType == data.filter1) {
-                filterRooms.push(rooms[i]);
-            }
-        }
-        if (data.filter2 != undefined) {
-            for (var i = filterRooms.length - 1; i >= 0; i--) {
-                if (filterRooms[i].data.status == data.filter2) {
-                } else {
-                    filterRooms.splice(i, 1);
-                }
-            }
-        }
-    } else {
-        if (data.filter2 != undefined) {
-            for (var i = rooms.length - 1; i >= 0; i--) {
-                if (rooms[i].data.status == data.filter2) {
-                } else {
-                    rooms.splice(i, 1);
-                }
-            }
-        }
-        filterRooms = rooms;
-    }
-
-    filterRooms = filterRooms.sort((a, b) => a.data.order - b.data.order)
-    res.render("pages/andar2", { rooms: filterRooms, filter: true });
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//ROTAS DE AÇÃO
 router.get('/sendRerervation/:id', async (req, res) => {
     let id = req.params.id;
+    //Recebe o usuário logado, reservas e salas
     let user = userApp;
     let reservations = await getReservations();
     let room = await getOneRoom(id)
+    //Variável auxiliar para envio de reserva
     let sendReservation;
+    // let sendStatus;
 
     if (reservations != null) {
         reservations.forEach(async reservation => {
             // Verifica se o usuário já está reservando alguma sala
             if (reservation.data.status == "PERMITIDO" && reservation.data.uid == user.uid) {
-                console.log("Você já está reservando uma sala!")
+                // sendStatus = "Você já está reservando uma sala!";
             } else {
                 sendReservation = {
                     uid: user.uid,
@@ -259,7 +115,10 @@ router.get('/sendRerervation/:id', async (req, res) => {
                     checkin: null,
                     checkout: null
                 }
+                //Reservando a sala
                 let response = await axios.post("http://localhost:8080/api/reserve", sendReservation)
+                //Status
+                // sendStatus = (response == "Reservation recorded :)") ? "Seu pedido de reserva foi enviado com sucesso, aguarde a confirmação!" : "Ocorreu um erro no seu pedido de reserva, tente novamente!";
             }
         });
     } else {
@@ -273,8 +132,11 @@ router.get('/sendRerervation/:id', async (req, res) => {
             checkin: null,
             checkout: null
         }
-        let response = await axios.post("http://localhost:8080/api/reserve", sendReservation)
+        let response = await axios.post("http://localhost:8080/api/reserve", sendReservation);
+        // sendStatus = (response == "Reservation recorded :)") ? "Seu pedido de reserva foi enviado com sucesso, aguarde a confirmação!" : "Ocorreu um erro no seu pedido de reserva, tente novamente!";
     }
+    res.redirect('/home');
+    // res.redirect('/home', { alert: sendReservation });
 })
 
 router.get('/finalizeReservation/:id', async (req, res) => {
@@ -295,6 +157,137 @@ router.get('/finalizeReservation/:id', async (req, res) => {
         status: "FINALIZADO"
     }
     let responseReservation = await updateReservation(id, dataUpdateReservation);
+
+});
+
+/* <<< ROTAS DE FILTROS >>> */
+
+router.post('/home', async (req, res) => {
+    let data = req.body
+    //Recebe salas
+    let rooms = await getRooms();
+    let filterRooms = [];
+    //Remove salas de outros andares
+    for (var i = rooms.length - 1; i >= 0; i--) {
+        if (rooms[i].data.position != 0) {
+            rooms.splice(i, 1);
+        }
+    }
+    //Filtra as salas de acordo com o filtro
+    if (data.filter1 != undefined) {
+        for (var i = rooms.length - 1; i >= 0; i--) {
+            if (rooms[i].data.roomType == data.filter1) {
+                filterRooms.push(rooms[i]);
+            }
+        }
+        if (data.filter2 != undefined) {
+            for (var i = filterRooms.length - 1; i >= 0; i--) {
+                if (filterRooms[i].data.status == data.filter2) {
+                } else {
+                    filterRooms.splice(i, 1);
+                }
+            }
+        }
+    } else {
+        if (data.filter2 != undefined) {
+            for (var i = rooms.length - 1; i >= 0; i--) {
+                if (rooms[i].data.status == data.filter2) {
+                } else {
+                    rooms.splice(i, 1);
+                }
+            }
+        }
+        filterRooms = rooms;
+    }
+    //Ordena as salas
+    filterRooms = filterRooms.sort((a, b) => a.data.order - b.data.order)
+    res.render("pages/home", { rooms: filterRooms, filter: true });
+
+});
+
+router.post('/andar1', async (req, res) => {
+    let data = req.body
+    //Recebe salas
+    let rooms = await getRooms();
+    let filterRooms = [];
+    //Remove salas de outros andares
+    for (var i = rooms.length - 1; i >= 0; i--) {
+        if (rooms[i].data.position != 1) {
+            rooms.splice(i, 1);
+        }
+    }
+    //Filtra as salas de acordo com o filtro
+    if (data.filter1 != undefined) {
+        for (var i = rooms.length - 1; i >= 0; i--) {
+            if (rooms[i].data.roomType == data.filter1) {
+                filterRooms.push(rooms[i]);
+            }
+        }
+        if (data.filter2 != undefined) {
+            for (var i = filterRooms.length - 1; i >= 0; i--) {
+                if (filterRooms[i].data.status == data.filter2) {
+                } else {
+                    filterRooms.splice(i, 1);
+                }
+            }
+        }
+    } else {
+        if (data.filter2 != undefined) {
+            for (var i = rooms.length - 1; i >= 0; i--) {
+                if (rooms[i].data.status == data.filter2) {
+                } else {
+                    rooms.splice(i, 1);
+                }
+            }
+        }
+        filterRooms = rooms;
+    }
+    //Ordena as salas
+    filterRooms = filterRooms.sort((a, b) => a.data.order - b.data.order)
+    res.render("pages/andar1", { rooms: filterRooms, filter: true });
+
+});
+
+router.post('/andar2', async (req, res) => {
+    let data = req.body
+    //Recebe as salas
+    let rooms = await getRooms();
+    let filterRooms = [];
+    //Remove salas de outros andares
+    for (var i = rooms.length - 1; i >= 0; i--) {
+        if (rooms[i].data.position != 2) {
+            rooms.splice(i, 1);
+        }
+    }
+    //Filtra as salas de acordo com o filtro
+    if (data.filter1 != undefined) {
+        for (var i = rooms.length - 1; i >= 0; i--) {
+            if (rooms[i].data.roomType == data.filter1) {
+                filterRooms.push(rooms[i]);
+            }
+        }
+        if (data.filter2 != undefined) {
+            for (var i = filterRooms.length - 1; i >= 0; i--) {
+                if (filterRooms[i].data.status == data.filter2) {
+                } else {
+                    filterRooms.splice(i, 1);
+                }
+            }
+        }
+    } else {
+        if (data.filter2 != undefined) {
+            for (var i = rooms.length - 1; i >= 0; i--) {
+                if (rooms[i].data.status == data.filter2) {
+                } else {
+                    rooms.splice(i, 1);
+                }
+            }
+        }
+        filterRooms = rooms;
+    }
+    //Ordena as salas
+    filterRooms = filterRooms.sort((a, b) => a.data.order - b.data.order)
+    res.render("pages/andar2", { rooms: filterRooms, filter: true });
 
 });
 
